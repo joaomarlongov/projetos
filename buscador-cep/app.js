be6c64c411610892170f32153bd996ef8d61a204
eumarlotemec <marlotejmct@gmail.com>
2023-09-27 17:01:29 -0300

    Add gerenciado-de-link

--- buscador-cep/app.js
+++ buscador-cep/app.js
@@ -1,46 +1,45 @@
-function buscarCEP(){
-    let input = document.getElementById('input').value;
-    let btnAdd = document.getElementById('btn-add');
-    let cepOut = document.getElementById('cepOut');
+function buscarCEP() {
+  let input = document.getElementById("input").value
+  let btnAdd = document.getElementById("btn-add")
+  let cepOut = document.getElementById("cepOut")
 
-    const ajax = new XMLHttpRequest();
-    ajax.open('GET', `https://viacep.com.br/ws/${input}/json/`);
-    ajax.send();
+  const ajax = new XMLHttpRequest()
+  ajax.open("GET", `https://viacep.com.br/ws/${input}/json/`)
+  ajax.send()
 
-    ajax.onload = function () {
-    let obj = JSON.parse(this.responseText);
+  ajax.onload = function () {
+    let obj = JSON.parse(this.responseText)
 
-    let logradouro = obj.logradouro;
-    let bairro = obj.bairro;
-    let localidade = obj.localidade;
-    let uf = obj.uf;
-    let ddd = obj.ddd;
+    let logradouro = obj.logradouro
+    let bairro = obj.bairro
+    let localidade = obj.localidade
+    let uf = obj.uf
+    let ddd = obj.ddd
 
-    let cepOut = document.getElementById('cepOut');
-   
+    let cepOut = document.getElementById("cepOut")
+
     cepOut.innerHTML = `
     <p id="logradouro">Logradouro: ${logradouro}</p>
     <p id="bairro">Bairro: ${bairro}</p>
     <p id="cidade">Cidade: ${localidade}</p>
     <p id="uf">UF: ${uf}</p>
     <p id="ddd">DDD: ${ddd}</p>
-    `;
-  };
+    `
+  }
 }
 
-function apagar(){
-    let cepOut = document.getElementById('cepOut');
-    cepOut.innerHTML = ``;
+function apagar() {
+  let cepOut = document.getElementById("cepOut")
+  cepOut.innerHTML = ``
 
-    input.value = ""
+  input.value = ""
 }
-    
 
-input.addEventListener("keyup", function(event){
-    if(event.keyCode===13){
-        event.preventDefault();
-        btnAdd.click();
-    }
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault()
+    btnAdd.click()
+  }
 })
 
 /* Bairro: ${obj.bairro}
