6b60b5a4523ceceafdd76569afff98cde5376a89
eumarlotemec <marlotejmct@gmail.com>
2023-09-28 12:09:32 -0300

    Add toggle dark-light mode

---
+++ gerenciador-de-link/js/app.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  // .toggle faz esse código abaixo...
+  /* if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
+
+}
